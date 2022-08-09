5b8e72003a23b85879a390e2bcd47ca13e5e6b13
Ashish Devkota <60335709+ashishdevkota@users.noreply.github.com>
2022-08-09 21:22:53 +0200

    textAnim
    textAnim file added

---
+++ textAnim.js
@@ -0,0 +1,27 @@
+
+let texts=['Freelancer','UI & UX Designer','IT OFFICER','PROGRAMMER','TECH GUY'];
+let demo=document.querySelector('#demo')
+let animation=gsap.timeline({repeat:-1,repeatDelay:0.6})
+
+
+function createLayers(){
+    texts.forEach(value => {
+        let layer=document.createElement('div');
+        layer.innerHTML=value;
+        demo.appendChild(layer);
+    })
+}
+
+
+//crate animation
+function animationText(){
+    let layers=document.querySelectorAll('#demo div');
+    layers.forEach(function(element,index){
+        animation.fromTo(element,{opacity:0,scale:0},{opacity:1,scale:1,repeat:1,yoyo:true,repeatDelay:0.3,yoyoEase:true})
+    })
+    gsap.set('#demo',{visibility:'visible'})
+}
+
+createLayers()
+
+animationText()
